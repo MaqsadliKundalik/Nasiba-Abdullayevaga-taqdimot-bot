bb60bb7ec5469dbc8ddf8bfca9b42dab22f0effa
Odilbek <=>
2025-11-07 20:29:09 +0500

    c

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ TOKEN = getenv("TOKEN")
 ADMIN = getenv("ADMIN")
 CARD_NUMBER = "9860010117403682"
 CARD_OWNER = "Muxtorova Nasiba"
-QUARTER_PRICE = 75_000  # Chorak uchun narx
+QUARTER_PRICE = 100_000  # Chorak uchun narx
